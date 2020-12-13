cd7515cc5ce562f20e019ea03ac41e2b5c9d5e30
Indiacodes <68460068+Indiacodes@users.noreply.github.com>
2020-12-13 11:31:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1440,980);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  //fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  //movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+
+  myBounceOff(movingRect,fixedRect);
+
+  drawSprites();
+}
+
